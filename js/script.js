f402f04abb1ec79378f020d5d46583ab681a8a08
Taciano06 <82884780+Taciano06@users.noreply.github.com>
2021-12-16 16:21:25 -0300

    Subindo projeto

---
+++ js/script.js
@@ -0,0 +1,81 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3'); 
+let botoes = document.querySelectorAll('[data-key]');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+  let etapa = etapas[etapaAtual];
+
+  let numeroHtml = '';
+  for (let i = 0; i < etapa.numeros; i++) {
+    if (i === 0) {
+      numeroHtml += '<div class="numero pisca"></div>';
+    } else {
+      numeroHtml += '<div class="numero"></div>';
+    }
+  }
+
+  seuVotoPara.style.display = 'none';
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = '';
+  aviso.style.display = 'none';
+  lateral.innerHTML = '';
+  numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+  let etapa = etapas[etapaAtual];
+  let candidato = etapa.candidatos.filter((item) => item.numero === numero ? true : false );
+
+  if (candidato.length > 0){
+    candidato = candidato[0];
+
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    descricao.innerHTML = `Nome: ${candidato.nome} <br/>Partido: ${candidato.partido}`;
+    let fotosHtml = '';
+    for (let i in candidato.fotos) {
+      fotosHtml +=  `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="vice-prefeito"><p>${candidato.fotos[i].legenda}</p></div>`;
+    }
+    lateral.innerHTML = fotosHtml;
+  } else {
+    seuVotoPara.style.display = 'block';
+    aviso.style.display = 'block';
+    descricao.innerHTML = `<div class="aviso--grande pisca">VOTO NULO</div>`;
+  }
+
+}
+
+function indentificandoBotao(e) {
+  let elNumero = document.querySelector('.numero.pisca');
+  let n = e.target.innerHTML;
+  if (elNumero !== null) {
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+
+    elNumero.classList.remove('pisca');
+    if (elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add('pisca');
+    } else {
+      atualizaInterface();
+    }
+  }
+}
+
+botoes.forEach((botao) => {
+  botao.addEventListener('click', indentificandoBotao)
+});
+
+comecarEtapa(); 
+
+
+
+
+
+
